e3c42ed50dc4a693c37a9e858194270b1ebd0c75
nhermann1 <nhermann@nd.edu>
2018-10-03 10:17:15 -0400

    Updated question 1 script with working pipe

--- question1.sh
+++ question1.sh
@@ -1,5 +1,7 @@
 #Answering Question 1
 #Shell script that returns the first two columns from a .csv file sorted--answer in answer1.txt
-#Usage: 
+#Usage: cat *.csv | bash question1.sh
+
+cut -d , -f 1,2 | tr "," " " | grep -v gender | sort -u -k1,1 -k2,2n -o answer1.txt
 
 
