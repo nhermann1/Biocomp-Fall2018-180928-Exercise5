5f58b2e0bfe73d1ce0ad882cc592114d9a6c9986
nhermann1 <nhermann@nd.edu>
2018-10-03 10:02:26 -0400

    Shell script for question 2

--- question2.sh
+++ question2.sh
@@ -1,4 +1,4 @@
 #Answering Question 2
-#Returns top and bottom earner info and count females in top 10--answer in stdout
+#Returns top and bottom earner info and counts females in top 10--answer in stdout
 #Usage:
 
