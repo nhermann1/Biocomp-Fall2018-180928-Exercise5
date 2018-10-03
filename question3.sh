ee369623238b7c715a072e53f65cb6f9faf3ee32
nhermann1 <nhermann@nd.edu>
2018-10-03 18:54:05 -0400

    Pseudo-code for the third question

--- question3.sh
+++ question3.sh
@@ -1,3 +1,8 @@
 #Answering Question 3
 #Effect of college graduation on income using difference--answer in stdout
-#Usage: 
+#Usage: bash question3.sh "filename"
+
+#Pseudo-code: define two variables, val1 and val2
+#val1 is equal to the average income of those with 16 years education, val2 those with 12 years education
+#find the average after grep-ing the two categories
+#echo the difference of the two variables defined above with bc 
