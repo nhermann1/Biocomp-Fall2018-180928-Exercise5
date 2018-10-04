f33fe3a17e1071f7753ecb5d7dd704a0b8201bb6
Will Phillips <wphilli2@nd.edu>
2018-10-04 16:21:01 -0400

    final answer to q3

--- question3.sh
+++ question3.sh
@@ -1,13 +1,11 @@
 #Answering Question 3
 #Effect of college graduation on income using difference--answer in stdout
 #Usage: 
-data=$(cat wages.csv | cut -d , -f 3,4 | grep ^1[62] | sort -nr -t , -k 1)
-val1=$(echo $data | grep ^16 | cut -d , -f 2 | sort -n | tail -n 1)
-val2=$(echo $data | grep ^12 | cut -d , -f 2 | sort -n | tail -n 1)
-echo "$val1"
-echo "$val2"
+data=$(cat wages.csv | cut -d , -f 3,4 | grep ^1[62])
+val1=$(echo "$data" | grep ^16 | cut -d , -f 2 | sort -nr | tail -n 1)
+val2=$(echo "$data" | grep ^12 | cut -d , -f 2 | sort -nr | tail -n 1)
+echo "$val1-$val2" | bc
+ 
 
 
-
-  
 
