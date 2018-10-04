8f1ef4458f4e0e5039f94da2c96c3fbb29fc9657
Will Phillips <wphilli2@nd.edu>
2018-10-04 15:32:15 -0400

    committing Nate's q3 changes

--- question3.sh
+++ question3.sh
@@ -1,3 +1,6 @@
 #Answering Question 3
 #Effect of college graduation on income using difference--answer in stdout
 #Usage: 
+for earner in {1,1600}
+do 
+cat "$1" | sort -nr -t , -k 4 | tail -n 1600
